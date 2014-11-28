87d11f7bd589c1e6ceb0bda7ff04c858f31cb984
Valerii Ovchinnikov <krems@xakep.ru>
2014-11-28 16:54:55 +0300

    Sessions cleaning up improved

---
+++ src/atm/server/Account.java
@@ -0,0 +1,60 @@
+package atm.server;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: shesdmi
+ * Date: 2/19/13
+ * Time: 3:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+
+    private double balance;
+    private String id;
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    public boolean transferTo(Account to, double delta) {
+        if(balance > delta) {
+            to.balance = to.balance + delta;
+            this.balance = this.balance - delta;
+            Thread.yield();
+            return true;
+        }
+        return false;
+    }
+
+    public void increase(double delta) {
+        balance = balance + delta;
+    }
+
+    public boolean withdraw(double delta) {
+        if(balance - delta > 0.00000001) {
+            balance = balance - delta;
+            return true;
+        }
+        return false;
+    }
+
+    public Account createCopy() {
+        Account res = new Account(id);
+        res.balance = balance;
+        return res;
+    }
+
+    public void rollbackToCopy(Account account) {
+        if(id.equals(account.id)) {
+            balance = account.balance;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ src/atm/server/StorageService.java
@@ -0,0 +1,49 @@
+package atm.server;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: shesdmi
+ * Date: 3/7/13
+ * Time: 5:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StorageService {
+    private final Map<String, Account> accountHashMap = new HashMap<>();
+    private final ConcurrentHashMap<String, Session> sessionHashMap = new ConcurrentHashMap<>(24, 0.75f, 2);
+
+    public Session createSessionById(String sessionId, String userId, String sourceId, byte[] credentials) {
+        Session session = new Session(getOrCreateAccount(userId), sessionId, sourceId, credentials);
+        sessionHashMap.put(sessionId, session);
+        return session;
+    }
+
+    public Session lookupSession(String sessionId) {
+        return sessionHashMap.get(sessionId);
+    }
+
+    public Session lookupSessionProxyForAccount(String accountId) {
+        return new Session(getOrCreateAccount(accountId), null, null, null);
+    }
+
+    public void cleanUpSession(String accountId, String sessionId) {
+        synchronized (accountHashMap) {
+            accountHashMap.remove(accountId);
+        }
+        sessionHashMap.remove(sessionId);
+    }
+
+    private Account getOrCreateAccount(String accountId) {
+        synchronized(accountHashMap) {
+            Account res = accountHashMap.get(accountId);
+            if (res == null) {
+                res = new Account(accountId);
+                accountHashMap.put(accountId, res);
+            }
+            return res;
+        }
+    }
+}
